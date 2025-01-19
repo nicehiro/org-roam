96f7a80f51cab3c9cf78db5b4c5316ec9aad7deb
fangyuan <fywang96@gmail.com>
2025-01-19 22:38:12 +0800

    new note on diffusion model

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((org-mode . ((eval . (org-indent-mode -1)))))
+((org-mode . ((org-download-image-dir . "./img")
+              (eval . (org-indent-mode -1)))))
