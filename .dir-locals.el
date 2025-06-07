ba946724f6051bde41ab58ac322c53401e03bfe5
fangyuan <fywang96@gmail.com>
2025-06-07 11:11:34 +0800

    add new paper note

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,8 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((org-mode . ((org-download-image-dir . "./img")
-              (eval . (org-indent-mode -1)))))
+              (eval . (org-indent-mode -1))
+              (eval . (org-hugo-auto-export-mode))
+              (org-hugo-base-dir . "/Users/fangyuan/Documents/quartz/")
+              (org-hugo-section . "/")
+              (org-hugo-from-matter-format . "yaml"))))
